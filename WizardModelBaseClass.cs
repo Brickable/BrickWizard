eb0b9989a1d9dda700135661d08a43b4ec854503
Joao Neves <jmgneves@hotmail.com>
2017-07-18 21:49:47 +0200

    ignore serialization of model base class

--- WizardModelBaseClass.cs
+++ WizardModelBaseClass.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace BrickWizard
 {
     public abstract class WizardModelBaseClass
     {
+        [IgnoreDataMember]
         public NavBar NavBar { get; internal set; }
+        [IgnoreDataMember]
         public Step PreviousStep { get; internal set; }
+        [IgnoreDataMember]
         public string ActionName { get; internal set; }
+        [IgnoreDataMember]
         public string ControllerName { get; internal set; }
     }
 }
