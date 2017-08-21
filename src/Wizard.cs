da8675eb3c0599dc8e344daa952385038196e5dd
Joao Neves <jmgneves@hotmail.com>
2017-08-21 08:15:26 +0200

    [Fix Issue #15] - Partial commit with compile errors - Separate concerns of sync method to 2 diferent commands: merge and sync

--- src/Wizard.cs
+++ src/Wizard.cs
@@ -30,10 +30,11 @@ namespace BrickWizard
         }
 
         
-
+        //PRIVATE FIELDS
         private string _controllerName { get; }
         private string _areaName { get; }
 
+        //PRIVATE PROPERTIES
         private Steps _steps { get; set; }
         private Map _map { get; set; }
         private List<Step> _currentRouteSteps
@@ -49,74 +50,82 @@ namespace BrickWizard
             }
         }
 
+        //OVERRIDABLE MEMBERS
         protected virtual int MaxTabs { get; } = 5;
         protected abstract Steps Steps { get; }
         protected abstract Map Map { get; }
 
+        //PUBLIC MEMBERS
         public Route CurrentRoute => _map.CurrentRoute;
         public Steps CurrentRouteSteps => new Steps(_currentRouteSteps);
         public Step CurrentStep => _steps.Current;
         public bool IsStepAvailable(string stepActionName)=> _steps.steps.Any(x => x.ActionName == stepActionName);
-
         public T Model { get; set; } = new T();
-        /// <summary>
-        /// Command that refresh the Model properties depending which object(s) you pass into. Analize your current wizard object state and decide what should be the next "move".
-        /// </summary>
-        /// <param name="obj">Source object to copy to the Model as destination</param>
-        /// 
-        public void Sync<T1>(T1 obj)
-        {
-            var callerMethodName = new StackTrace().GetFrame(1).GetMethod().Name;
-            var currentStep = CurrentStep.ActionName;
 
-            if (callerMethodName != currentStep)
+        //PUBLIC COMMANDS
+        public void AutoSync()
+        {
+            if (TryBackwardsMove(new StackTrace().GetFrame(1).GetMethod().Name))
             {
-                MoonWalkPerformed = true;
-                MoonWalkTill(callerMethodName);
-            }
-            else
-            {
-                MoonWalkPerformed = (obj == null);
-                if (obj != null)
+                MoveNext();
+            }            
+        }
+        public bool TryMoveBackwards() => TryBackwardsMove(new StackTrace().GetFrame(1).GetMethod().Name);
+        public void TryForceCommit(params object[] objs)
+        {
+            if (IsMoonWalkNeeded(new StackTrace().GetFrame(1).GetMethod().Name) && objs != null)
+            {                
+                foreach (var obj in objs)
                 {
                     var propertyInfo = typeof(T).GetProperties().First(x => x.PropertyType.FullName == obj.GetType().FullName);
                     var property = typeof(T).GetProperty(propertyInfo.Name);
                     property.SetValue(Model, obj);
-                }
-                Orchestrate();
+                }      
             }
-            BaseModelSync();
         }
-        /// <summary>
-        /// Command that refresh the Model properties depending which object(s) you pass into. Analize your current wizard object state and decide what should be the next "move".
-        /// </summary>
-        /// <param name="obj">set of Source objects to copy to the Model as destination</param>
-        public void Sync(params object[] objs)
+        public void TryCommit(T model)
         {
-            var callerMethodName = new StackTrace().GetFrame(1).GetMethod().Name;
-            var currentStep = CurrentStep.ActionName;
-
-            if (callerMethodName != currentStep)
+            if (IsMoonWalkNeeded(new StackTrace().GetFrame(1).GetMethod().Name))
             {
-                MoonWalkPerformed = true;
-                MoonWalkTill(callerMethodName);
-            }
-
-            else
-            {
-                MoonWalkPerformed = (objs == null || objs.Length == 0);
-                if (objs != null && objs.Length > 0)
+                foreach(var i in CurrentStep.PropertiesToBind ?? Enumerable.Empty<string>())
                 {
-                    foreach (var obj in objs)
-                    {
-                        var propertyInfo = typeof(T).GetProperties().First(x => x.PropertyType.FullName == obj.GetType().FullName);
-                        var property = typeof(T).GetProperty(propertyInfo.Name);
-                        property.SetValue(Model, obj);
-                    }
+
                 }
-                Orchestrate();
+
+                //foreach (var step in _steps.steps)
+                //{
+                //    if (_currentRouteSteps.FirstOrDefault(x => x.ActionName == step.ActionName) == null)
+                //    {
+                //        foreach (var prop in typeof(T).GetProperties())
+                //        {
+                //            foreach (var i in step.PropertiesToBind)
+                //            {
+                //                if (i.ToString() == prop.Name)
+                //                {
+                //                    var property = typeof(T).GetProperty(prop.Name);
+                //                    property.SetValue(Model, null);
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
+                //foreach (var obj in objs)
+                //{
+                //    var propertyInfo = typeof(T).GetProperties().First(x => x.PropertyType.FullName == obj.GetType().FullName);
+                //    var property = typeof(T).GetProperty(propertyInfo.Name);
+                //    property.SetValue(Model, obj);
+                //}
             }
-            BaseModelSync();
+        }
+        public void CommitAndAutoSync(T model)
+        {
+            TryCommit(model);
+            AutoSync();
+        }
+        public void CommitAndAutoSync(params object[] objs)
+        {
+            TryForceCommit(objs);
+            AutoSync();
         }
         public void ClearUnusedSteps()
         {
@@ -139,9 +148,18 @@ namespace BrickWizard
             }
         }
 
-        private Step NextStep => GetNeighbors(1);
-        private Step PreviousStep => GetNeighbors(-1);
-        private Step GetNeighbors(int i) => _currentRouteSteps.ElementAtOrDefault(_currentRouteSteps.FindIndex(x => x.Current) + i);
+        //PRIVATE MEMBERS & COMMANDS
+        private bool TryBackwardsMove(string callerMethodName)
+        {
+            var isMoonWalkNeeded = IsMoonWalkNeeded(callerMethodName);
+            if (IsMoonWalkNeeded(callerMethodName))
+            {
+                MoonWalkTill(callerMethodName);
+                BaseModelSync();
+            }
+            return isMoonWalkNeeded;
+        }
+        private bool IsMoonWalkNeeded(string callerMethodName) => (callerMethodName != CurrentStep.ActionName);
         private bool TryMoveNextStep() => TryIterateStep(true);
         private bool TryMovePreviousStep() => TryIterateStep(false);
         private bool TryIterateStep(bool isToIncrement)
@@ -156,7 +174,6 @@ namespace BrickWizard
             return true;
         }
         private bool IsTriggerPointStep => CurrentStep.TriggerPointRule != null;
-        private bool MoonWalkPerformed { get; set; }
         private void CleanRoutes() => _map.Routes.ForEach(x => x.Current = false);
         private void SetCoordinatesAt(int routeId, string actionName)
         {
@@ -178,7 +195,7 @@ namespace BrickWizard
             SetCoordinatesAt(routeId, CurrentStep.ActionName);
             TryMoveNextStep();
         }
-        private void Orchestrate()
+        private void MoveNext()
         {
             if (!IsTriggerPointStep)
             {
@@ -189,6 +206,52 @@ namespace BrickWizard
                 FollowTheRoute(CurrentStep.TriggerPointRule.Invoke());
             }
         }
+        private void BaseModelSync()
+        {
+            this.Model.NavBar = GetNavBar();
+            this.Model.ActionName = CurrentStep.ActionName;
+            this.Model.CurrentRouteId = CurrentRoute.RouteId;
+            this.Model.PreviousStepActionName = PreviousStep?.ActionName;
+            this.Model.ControllerName = _controllerName;
+            this.Model.AreaName = _areaName;
+        }
+        private void MoonWalkTill(string methodName)
+        {
+            while (CurrentStep.ActionName != methodName)
+            {
+                if (!TryMovePreviousStep())
+                    break;
+            }
+        }
+        private int GetNavBarStatingPointIndex()
+        {
+            var currentStep = CurrentRoute.RouteSteps.FirstOrDefault(x => x.ActionName == CurrentStep.ActionName);
+            var lastStep = CurrentRoute.RouteSteps.Last();
+
+            var currentStepIndex = CurrentRoute.RouteSteps.IndexOf(currentStep);
+            var lastStepIndex = CurrentRoute.RouteSteps.IndexOf(lastStep);
+            var middleIndex = Convert.ToInt16(MaxTabs / 2) + 1;
+
+            //Navbar has 3 behaviors: {GoingTowardsMiddle, Rolling, GoingTowardsEnd}
+            var GoingTowardsEnd = currentStepIndex + middleIndex > lastStepIndex;
+            var GoingTowardsMiddle = currentStepIndex < middleIndex;
+
+            if (GoingTowardsEnd)
+            {
+                return lastStepIndex - (MaxTabs - 1);
+            }
+            else if (GoingTowardsMiddle)
+            {
+                return 0;
+            }
+            else
+            {
+                return currentStepIndex - (middleIndex - 1);
+            }
+        }
+        private Step NextStep => GetNeighbors(1);
+        private Step PreviousStep => GetNeighbors(-1);
+        private Step GetNeighbors(int i) => _currentRouteSteps.ElementAtOrDefault(_currentRouteSteps.FindIndex(x => x.Current) + i);
         private List<StepReference> StepsToShow()
         {
             if (CurrentRoute.Lenght <= MaxTabs)
@@ -209,34 +272,6 @@ namespace BrickWizard
                 return stepsToShow;
             }
         }
-
-        private int GetNavBarStatingPointIndex()
-        {
-            var currentStep = CurrentRoute.RouteSteps.FirstOrDefault(x => x.ActionName == CurrentStep.ActionName);
-            var lastStep = CurrentRoute.RouteSteps.Last();
-
-            var currentStepIndex = CurrentRoute.RouteSteps.IndexOf(currentStep);
-            var lastStepIndex = CurrentRoute.RouteSteps.IndexOf(lastStep);
-            var middleIndex = Convert.ToInt16(MaxTabs / 2) + 1;
-
-            //Navbar has 3 behaviors: {GoingTowardsMiddle, Rolling, GoingTowardsEnd}
-            var GoingTowardsEnd = currentStepIndex + middleIndex > lastStepIndex;
-            var GoingTowardsMiddle = currentStepIndex < middleIndex;
-
-            if (GoingTowardsEnd)
-            {
-                return lastStepIndex - (MaxTabs-1);
-            }
-            else if (GoingTowardsMiddle)
-            {
-                return 0;
-            }
-            else
-            {
-                return currentStepIndex - (middleIndex-1);
-            }
-        }
-
         private NavBar GetNavBar()
         {
             var navBarList = new List<Tab>();
@@ -262,23 +297,6 @@ namespace BrickWizard
             }
             var maxTabs = (MaxTabs >= CurrentRoute.Lenght) ? MaxTabs : CurrentRoute.Lenght;
             return new NavBar(maxTabs, navBarList, _controllerName, _areaName);
-        }
-        private void BaseModelSync()
-        {
-            this.Model.NavBar = GetNavBar();
-            this.Model.ActionName = CurrentStep.ActionName;
-            this.Model.CurrentRouteId = CurrentRoute.RouteId;
-            this.Model.PreviousStepActionName = PreviousStep?.ActionName;
-            this.Model.ControllerName = _controllerName;
-            this.Model.AreaName = _areaName;
-        }
-        private void MoonWalkTill(string methodName)
-        {
-            while (CurrentStep.ActionName != methodName)
-            {
-                if (!TryMovePreviousStep())
-                    break;
-            }
         }
     }
 }
